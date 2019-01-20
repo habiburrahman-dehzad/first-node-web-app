b3d74a46c8df6284c9cd5ab533ea31d245da4724
Habiburrahman Dehzad <rhabiblv@gmail.com>
2019-01-20 16:38:22 +0430

    Initial Commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/Views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to log in to server.log.');
+        }
+    });
+
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Maintenance Ongoing'
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Node Web Server',
+        welcomeText: 'Welcome to First Node Web site'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Node Web Server'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.status(404).send('Unable to process the request');
+});
+
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000');
+});
