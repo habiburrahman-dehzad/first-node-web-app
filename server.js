92ff4228c71e15d7362bca9db42d7bb6c2a2a980
Habiburrahman Dehzad <rhabiblv@gmail.com>
2019-01-28 14:16:14 +0430

    Removed the site from maintenance state and into running state

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        pageTitle: 'Maintenance Ongoing'
-    });
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance Ongoing'
+//     });
+// });
 
 app.use(express.static(__dirname + '/public'));
 
