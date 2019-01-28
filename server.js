12965496acd38ce77ea07766f5b24d34223a8579
Habiburrahman Dehzad <rhabiblv@gmail.com>
2019-01-28 14:23:49 +0430

    Fixed the folder name to use lowercase letters for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-hbs.registerPartials(__dirname + '/Views/partials');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
